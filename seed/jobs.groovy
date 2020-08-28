c45f26d8ad19ed573e959fbb7c701d484f6eada2
Mahaboob Basha <mahaboobbasha2109@gmail.com>
2020-08-28 16:11:32 +0530

    Jobs.groovy

--- seed/jobs.groovy
+++ seed/jobs.groovy
@@ -5,7 +5,7 @@ pipelineJob("Cart-CI") {
       scm {
         git {
           remote {
-            url("https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps49/_git/rs-cart")
+            url("https://github.com/mahaboobbasha2109/roboshop.git")
           }
           branch("*/master")
         }
@@ -22,7 +22,7 @@ pipelineJob("Catalogue-CI") {
       scm {
         git {
           remote {
-            url("https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps49/_git/rs-catalogue")
+            url("https://github.com/mahaboobbasha2109/roboshop.git")
           }
           branch("*/master")
         }
